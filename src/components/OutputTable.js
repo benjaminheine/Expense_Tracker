4a60647810f48cce140f3e637d32f489628fb208
benjaminheine <benjaminheine@gmx.net>
2019-11-14 20:52:42 +0100

    First steps with react, creating some layout

---
+++ src/components/OutputTable.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function OutputTable() {
+  return (
+    <div>
+      <table className="Table-input">
+        <tr>
+          <th>type</th>
+          <th>name</th>
+          <th>date</th>
+          <th>amount</th>
+        </tr>
+        <tr>
+          <td>cash</td>
+          <td>mc d</td>
+          <td>14.11.2019</td>
+          <td>50 Euro</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+ export default OutputTable;
