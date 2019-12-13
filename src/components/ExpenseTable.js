26a00e64a511dd86bc43bb973b9cb071b1bfa44a
benjaminheine <benjaminheine@gmx.net>
2019-12-13 17:37:51 +0100

    deleted package-jason.loc

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ExpenseRow from './ExpenseRow';
+import React from "react";
+import ExpenseRow from "./ExpenseRow";
 
-function ExpenseTable (props) {
-    let expenseRows = props.expenses.map((expense, index) => (
-        <ExpenseRow
-          key={index}
-          expense={expense}
-          deleteExpenseByCheckbox={props.deleteExpense}
-        />
-      ));
-    return(
-        <table id="expense-table" className="table-input">
-          <tbody>
-            <tr>
-              <th>type</th>
-              <th>name</th>
-              <th>date</th>
-              <th>amount</th>
-            </tr>
-            {expenseRows}
-          </tbody>
-        </table>
-    )
+function ExpenseTable(props) {
+  let expenseRows = props.expenses.map((expense, index) => (
+    <ExpenseRow
+      key={index}
+      expense={expense}
+      deleteExpenseByCheckbox={props.deleteExpense}
+    />
+  ));
+  return (
+    <table id="expense-table" className="table-input">
+      <tbody>
+        <tr>
+          <th>type</th>
+          <th>name</th>
+          <th>date</th>
+          <th>amount</th>
+        </tr>
+        {expenseRows}
+      </tbody>
+    </table>
+  );
 }
 
 export default ExpenseTable;
